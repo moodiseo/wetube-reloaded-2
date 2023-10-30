512f9c323b03ff9430ac9c314f4db08bb550e140
slqkealr <135933231+slqkealr@users.noreply.github.com>
2023-10-30 17:24:39 +0900

    MVP Styles

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,9 @@
-export const trendig = (req, res) => res.render("home");
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+export const trendig = (req, res) =>
+  res.render("home", { pageTitle: "Comes from your controller" });
+export const see = (req, res) =>
+  res.render("watch", { pageTitle: "Watch Video" });
+export const edit = (req, res) =>
+  res.render("edit", { pageTitle: "Edit Video" });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
