8bdc282d98d9d4bd50e0a09f99a5581a41a29dc6
slqkealr <135933231+slqkealr@users.noreply.github.com>
2023-10-31 18:22:10 +0900

    Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,34 @@
-const fakeUser = {
-  username: "Dabin",
-  loggedIn: true,
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trendig = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
-export const see = (req, res) =>
-  res.render("watch", { pageTitle: "Watch Video" });
-export const edit = (req, res) =>
-  res.render("edit", { pageTitle: "Edit Video" });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
