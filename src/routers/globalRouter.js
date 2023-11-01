c24f969fb44c5d7ea08b5b9075d04fdf61b7d7ca
slqkealr <135933231+slqkealr@users.noreply.github.com>
2023-11-01 16:29:36 +0900

    Array Database

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { trendig, search } from "../controllers/videoController";
+import { trending, search } from "../controllers/videoController";
 import { join, login } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
-globalRouter.get("/", trendig);
+globalRouter.get("/", trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
 globalRouter.get("/search", search);
