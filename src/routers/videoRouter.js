b5ece2e59930fc176205a1cc666672b6673077ce
moodiseo <moodiseo@gmail.com>
2023-11-17 16:05:26 +0900

    Edit Video part Two

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,16 +1,9 @@
 import express from "express";
-import {
-  watch,
-  getEdit,
-  upload,
-  deleteVideo,
-  postEdit,
-} from "../controllers/videoController";
+import { watch, getEdit, postEdit } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", getEdit);
-videoRouter.post("/:id(\\d+)/edit", postEdit);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
 export default videoRouter;
